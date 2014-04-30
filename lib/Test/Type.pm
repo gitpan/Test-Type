b65d4c1c55700c525aefd14f1dd560edfd779951
Guillaume Aubert <aubertg@cpan.org>
2014-04-30 04:12:12 +0000

    Import of AUBERTG/Test-Type-v1.2.0 from CPAN.
    gitpan-cpan-distribution: Test-Type
    gitpan-cpan-version:      v1.2.0
    gitpan-cpan-path:         AUBERTG/Test-Type-v1.2.0.tar.gz
    gitpan-cpan-author:       AUBERTG
    gitpan-cpan-maturity:     released

--- lib/Test/Type.pm
+++ lib/Test/Type.pm
@@ -16,11 +16,11 @@ Test::Type - Functions to validate data types in test files.
 
 =head1 VERSION
 
-Version 1.1.3
+Version 1.2.0
 
 =cut
 
-our $VERSION = '1.1.3';
+our $VERSION = '1.2.0';
 
 
 =head1 SYNOPSIS
@@ -73,6 +73,13 @@ our $VERSION = '1.1.3';
 		class => $class,
 	);
 
+	# Test regular expressions.
+	ok_regex( $variable );
+	ok_regex(
+		$variable,
+		name => 'Test regular expression',
+	);
+
 =cut
 
 our @EXPORT = ## no critic (Modules::ProhibitAutomaticExportation)
@@ -83,6 +90,7 @@ our @EXPORT = ## no critic (Modules::ProhibitAutomaticExportation)
 	'ok_instance',
 	'ok_number',
 	'ok_string',
+	'ok_regex',
 );
 
 
@@ -481,6 +489,31 @@ sub ok_instance
 			class => $class,
 		),
 		$name . ' is an instance of ' . $class . '.',
+	);
+}
+
+
+=head2 ok_regex()
+
+Test if the variable is a regular expression.
+
+	ok_regex( $variable );
+
+=cut
+
+sub ok_regex
+{
+	my ( $variable, %args ) = @_;
+
+	# Verify arguments and set defaults.
+	my $name = delete( $args{'name'} );
+	$name = 'Variable' if !defined( $name );
+	Carp::croak( 'Unknown parameter(s): ' . join( ', ', keys %args ) . '.' )
+		if scalar( keys %args ) != 0;
+
+	return Test::More::ok(
+		Data::Validate::Type::is_regex( $variable ),
+		$name . ' is a regular expression.',
 	);
 }
 
